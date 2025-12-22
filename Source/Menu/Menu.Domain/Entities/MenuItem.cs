bcb227f10f7e621985bdd53ea354a2a723efcdc1
Nicolas de Campos Fischer <79231180+NicolasFischer2002@users.noreply.github.com>
2025-12-21 21:17:57 -0300

    New fields, validations, and behaviors for the MenuItem entity.  New Entity: MenuItemImage

--- Source/Menu/Menu.Domain/Entities/MenuItem.cs
+++ Source/Menu/Menu.Domain/Entities/MenuItem.cs
@@ -1,12 +1,19 @@
 ﻿using Menu.Domain.Enums;
+using Menu.Domain.Exceptions;
 using Menu.Domain.ValueObjects;
 
 namespace Menu.Domain.Entities
 {
+    // Berlim => Continuar daqui:
+    // Adicionar os validações e comportamentos das listas _itemImages e _ingredients
+    // Adicionar validações de nullable para o ctor da lista da entidade Order.
     public sealed class MenuItem
     {
         public Guid Id { get; init; }
-        public Category Category { get; private set; }
+        private List<Category> _categories { get; set; }
+        public IReadOnlyCollection<Category> Categories => _categories.AsReadOnly();
+        private List<MenuItemImage> _itemImages { get; set; }
+        public IReadOnlyCollection<MenuItemImage> ItemImages => _itemImages.AsReadOnly();
         public ItemName Name { get; private set; }
         public Price Price { get; private set; }
         private List<Ingredient> _ingredients { get; set; }
@@ -15,21 +22,110 @@ namespace Menu.Domain.Entities
         public bool OptionalItem { get; private set; }
         public bool ItemIsActive { get; private set; }
 
-        public MenuItem(Guid id, Category category, ItemName name, Price price, 
-            List<Ingredient> ingredients, PreparationTimeInMinutes? preparationTimeInMinutes,
-            bool optionalItem, bool itemIsActive)
+        public MenuItem(Guid id, List<Category> categories, List<MenuItemImage> itemImages, 
+            ItemName name, Price price, List<Ingredient>? ingredients,
+            PreparationTimeInMinutes? preparationTimeInMinutes, bool optionalItem, bool itemIsActive)
         {
             Id = id;
-            Category = category;
-            Name = name; 
+
+            _categories = categories ?? throw new MenuException(
+                "A coleção de categorias não pode ser nula.",
+                string.Empty
+            );
+
+            _itemImages = itemImages ?? throw new MenuException(
+                "A coleção de imagens do item do menu não pode ser nula.",
+                string.Empty
+            );
+
+            Name = name;
             Price = price;
-            _ingredients = ingredients;
+            _ingredients = ingredients ?? new List<Ingredient>();
             PreparationTimeInMinutes = preparationTimeInMinutes;
             OptionalItem = optionalItem;
             ItemIsActive = itemIsActive;
+
+            ValidateCategories();
+            ValidateItemImages();
         }
 
-        public void UpdateItemCategory(Category newCategory) => Category = newCategory;
+        private void ValidateItemImages()
+        {
+            EnsureImageCountWithinLimit();
+            EnsureHasPrimaryImage();
+            EnsureSinglePrimaryImage();
+        }
+
+        private void EnsureImageCountWithinLimit()
+        {
+            const int MaximumNumberOfImages = 3;
+
+            if (_itemImages.Count > MaximumNumberOfImages)
+            {
+                throw new MenuException(
+                    $"O número máximo permitido de imagens por item do menu é de {MaximumNumberOfImages}.",
+                    _itemImages.Count.ToString()
+                );
+            }
+        }
+
+        private void EnsureHasPrimaryImage()
+        {
+            if (!_itemImages.Any(i => i.IsPrimary))
+            {
+                throw new MenuException(
+                    "Uma imagem deve ser marcada como principal para este item do menu.",
+                    string.Empty
+                );
+            }
+        }
+
+        private void EnsureSinglePrimaryImage()
+        {
+            const int MaximumNumberOfPrimaryImages = 1;
+
+            var primaryCount = _itemImages.Count(i => i.IsPrimary);
+            if (primaryCount > MaximumNumberOfPrimaryImages)
+            {
+                throw new MenuException(
+                    "Apenas uma imagem pode estar marcada como principal por item do menu.",
+                    primaryCount.ToString()
+                );
+            }
+        }
+
+        private void ValidateCategories()
+        {
+            if (_categories.Count == 0)
+            {
+                throw new MenuException(
+                    $"O item do menu '{Name}' precisa possuir ao menos uma categoria.",
+                    string.Empty
+                );
+            }
+        }
+
+        public void AddCategory(Category category)
+        {
+            if (!_categories.Contains(category))
+            {
+                _categories.Add(category);
+            }
+        }
+
+        public void RemoveCategory(Category category)
+        {
+            if (_categories.Count == 1)
+            {
+                throw new MenuException(
+                    "O item do menu deve possui ao menos uma categoria",
+                    string.Empty
+                );
+            }
+
+            _categories.Remove(category);
+        }
+
         public void UpdateItemName(string newName) => Name = new ItemName(newName);
         public void UpdatePrice(decimal newPrice) => Price = new Price(newPrice);
         public void UpdatePreparationTimeInMinutes(int preparationTimeInMinutes)
---
+++ Source/Menu/Menu.Domain/Entities/MenuItemImage.cs
@@ -0,0 +1,21 @@
+﻿namespace Menu.Domain.Entities
+{
+    public sealed class MenuItemImage
+    {
+        public Guid Id { get; init; }
+        public Uri Url { get; private set; }
+        public string ContentType { get; private set; }
+        public bool IsPrimary { get; private set; }
+
+        internal MenuItemImage(Guid id, Uri url, string contentType, bool isPrimary)
+        {
+            Id = id;
+            Url = url;
+            ContentType = contentType;
+            IsPrimary = isPrimary;
+        }
+
+        internal void MarkAsPrimary() => IsPrimary = true;
+        internal void UnmarkAsPrimary() => IsPrimary = false;
+    }
+}
