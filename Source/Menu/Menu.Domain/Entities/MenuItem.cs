4641468772b5b0882e5842e077547ae3a43f4f30
Nicolas de Campos Fischer <79231180+NicolasFischer2002@users.noreply.github.com>
2025-12-22 21:58:16 -0300

    New behaviors related to Menu images have been added.

--- Source/Menu/Menu.Domain/Entities/MenuItem.cs
+++ Source/Menu/Menu.Domain/Entities/MenuItem.cs
@@ -7,6 +7,7 @@ namespace Menu.Domain.Entities
     // Berlim => Continuar daqui:
     // Adicionar os validações e comportamentos das listas _itemImages e _ingredients
     // Adicionar validações de nullable para o ctor da lista da entidade Order.
+    // Avaliar se é possível melhorar o método AddImageReplacingThePrimaryOne
     public sealed class MenuItem
     {
         public Guid Id { get; init; }
@@ -21,6 +22,7 @@ namespace Menu.Domain.Entities
         public PreparationTimeInMinutes? PreparationTimeInMinutes { get; private set; }
         public bool OptionalItem { get; private set; }
         public bool ItemIsActive { get; private set; }
+        private const int MaximumNumberOfImages = 3;
 
         public MenuItem(Guid id, List<Category> categories, List<MenuItemImage> itemImages, 
             ItemName name, Price price, List<Ingredient>? ingredients,
@@ -54,12 +56,11 @@ namespace Menu.Domain.Entities
             EnsureImageCountWithinLimit();
             EnsureHasPrimaryImage();
             EnsureSinglePrimaryImage();
+            EnsureThatTheImagesHaveUniqueUrls();
         }
 
         private void EnsureImageCountWithinLimit()
         {
-            const int MaximumNumberOfImages = 3;
-
             if (_itemImages.Count > MaximumNumberOfImages)
             {
                 throw new MenuException(
@@ -71,7 +72,7 @@ namespace Menu.Domain.Entities
 
         private void EnsureHasPrimaryImage()
         {
-            if (!_itemImages.Any(i => i.IsPrimary))
+            if (!_itemImages.Any(ii => ii.IsPrimary))
             {
                 throw new MenuException(
                     "Uma imagem deve ser marcada como principal para este item do menu.",
@@ -84,12 +85,30 @@ namespace Menu.Domain.Entities
         {
             const int MaximumNumberOfPrimaryImages = 1;
 
-            var primaryCount = _itemImages.Count(i => i.IsPrimary);
+            var primaryCount = _itemImages.Count(ii => ii.IsPrimary);
             if (primaryCount > MaximumNumberOfPrimaryImages)
             {
                 throw new MenuException(
                     "Apenas uma imagem pode estar marcada como principal por item do menu.",
                     primaryCount.ToString()
+                );
+            }
+        }
+
+        private void EnsureThatTheImagesHaveUniqueUrls()
+        {
+            List<Uri>? repeatedImagesOfMenuItems = _itemImages
+                .GroupBy(ii => ii.Url)
+                .Where(g => g.Count() > 1)
+                .Select(g => g.Key)
+                .ToList();
+
+            if (repeatedImagesOfMenuItems.Any())
+            {
+                var duplicatedUrls = string.Join(", ", repeatedImagesOfMenuItems);
+                throw new MenuException(
+                    $"As imagens adicionadas ao item do menu estão repetidas: {duplicatedUrls}",
+                    string.Empty
                 );
             }
         }
@@ -136,5 +155,27 @@ namespace Menu.Domain.Entities
         public void UnmarkAsOptional() => OptionalItem = false;
         public void ActivateItem() => ItemIsActive = true;
         public void DeactivateItem() => ItemIsActive = false;
+
+        public void AddImageReplacingThePrimaryOne(MenuItemImage newImage)
+        {
+            try
+            {
+                if (newImage.IsPrimary)
+                {
+                    _itemImages.RemoveAll(ii => ii.IsPrimary);
+                }
+
+                _itemImages.Add(newImage);
+                ValidateItemImages();
+            }
+            catch (Exception ex)
+            {
+                _itemImages.Remove(newImage);
+                throw new MenuException(
+                    $"Não foi possível adicionar a nova imagem ao item do menu. {ex.Message}",
+                    ex.Message
+                );
+            }
+        }
     }
 }
